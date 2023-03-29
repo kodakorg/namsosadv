3692e9bea3371e81057c8db1da04133652f79116
Ole Severin Hustad <ole.severin.hustad@basefarm.com>
2023-03-29 15:48:23 +0200

    Remove redesign

--- server.js
+++ server.js
@@ -109,8 +109,7 @@ app.post('/skjema', function (req, res) {
     console.log("epostkopi: " + epostkopi);
     const mailOptions = {
       from: '"namsos advokat" <namsosadv@gmail.com>',
-      //to: "br@namsosadvokatene.no",
-      to: "ole.hustad@gmail.com",
+      to: "br@namsosadvokatene.no",
       replyTo: epost,
       subject: "Kontaktskjema Namsosadvokatene",
       text: html_string,
@@ -130,112 +129,6 @@ app.post('/skjema', function (req, res) {
       } else {
         transporter.close();
         res.render('pages/tilbakemelding', { sjekk: true })
-      }
-    })
-  }
-});
-
-////////////////
-// REDESIGN   //
-////////////////
-app.get('/redesign', function (req, res) {
-  res.render('redesign/pages/hovedside');
-});
-
-app.get('/redesign/kompetanse', function (req, res) {
-  res.render('redesign/pages/kompetanse');
-});
-
-app.get('/redesign/omoss', function (req, res) {
-  res.render('redesign/pages/omoss');
-});
-
-app.get('/redesign/priser', function (req, res) {
-  res.render('redesign/pages/priser');
-});
-
-app.get('/redesign/forsikring', function (req, res) {
-  res.render('redesign/pages/forsikring');
-});
-
-app.get('/redesign/kontakt', function (req, res) {
-  res.render('redesign/pages/kontakt', {
-    sjekk: false,
-    message: null
-  })
-});
-
-app.get('/redesign/kontaktskjema', function (req, res) {
-  res.render('redesign/pages/kontaktskjema', {
-    sjekk: false,
-    message: null
-  })
-});
-
-app.post('/redesign/skjema', function (req, res) {
-  var navn = req.body.navn;
-  var bosted = req.body.bosted;
-  var epost = req.body.epost;
-  var tlf = req.body.tlf;
-  var tekst = req.body.tekst;
-  var epostkopi = req.body['epostkopi'];
-  var html_string = "";
-
-  html_string += "Navn: " + navn + "<br><br>";
-  html_string += "Bosted: " + bosted + "<br><br>";
-  html_string += "Epost: " + epost + "<br>";
-  html_string += "Telefonnummer: " + tlf + "<br><br>";
-  html_string += "Forespørsel: " + tekst;
-
-  if (typeof navn === 'undefined' || navn === null || navn === '') {
-    res.render('redesign/pages/tilbakemelding', {
-      sjekk: false,
-      message: "Fornavn mangler eller er tom"
-    })
-  } else if (typeof bosted === 'undefined' || bosted === null || bosted === '') {
-    res.render('redesign/pages/tilbakemelding', {
-      sjekk: false,
-      message: "Etternavn mangler eller er tom"
-    })
-  } else if (!validateEmail(epost)) {
-    res.render('redesign/pages/tilbakemelding', {
-      sjekk: false,
-      message: "Epost er feil formatert"
-    })
-  } else if (typeof tlf === 'undefined' || tlf === null || tlf === '') {
-    res.render('redesign/pages/tilbakemelding', {
-      sjekk: false,
-      message: "Telefonnummer mangler"
-    })
-  } else if (typeof tekst === 'undefined' || tekst === null || tekst === '') {
-    res.render('redesign/pages/tilbakemelding', {
-      sjekk: false,
-      message: "Forespørsel er tom"
-    })
-  } else {
-    console.log("epostkopi: " + epostkopi);
-    const mailOptions = {
-      from: '"namsos advokat" <namsosadv@gmail.com>',
-      to: "br@namsosadvokatene.no",
-      replyTo: epost,
-      subject: "Kontaktskjema Namsosadvokatene",
-      text: html_string,
-      html: "<b>" + html_string + "</b>",
-    }
-
-    if (epostkopi) {
-      mailOptions.cc = epost;
-    }
-
-    transporter.sendMail(mailOptions, function (err, result) {
-      if (err) {
-        res.render('redesign/pages/tilbakemelding', {
-          sjekk: false,
-          message: err
-        })
-      } else {
-        transporter.close();
-        res.render('redesign/pages/tilbakemelding', { sjekk: true })
       }
     })
   }
